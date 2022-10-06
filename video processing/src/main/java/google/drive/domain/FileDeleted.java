bf344fecc122b8c8c8d0d8a40c44bdc1a9d08648
chlgustn86 <73877464+chlgustn86@users.noreply.github.com>
2022-10-06 15:39:04 +0900

    commit

---
+++ video processing/src/main/java/google/drive/domain/FileDeleted.java
@@ -0,0 +1,18 @@
+package google.drive.domain;
+
+import google.drive.domain.*;
+import google.drive.infra.AbstractEvent;
+import lombok.*;
+import java.util.*;
+@Data
+@ToString
+public class FileDeleted extends AbstractEvent {
+
+    private Long id;
+    private String name;
+    private String type;
+    private Long size;
+    private String status;
+}
+
+
